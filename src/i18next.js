86e8a782b7687d8252461c06c0ffc10628dccfc4
cryptojavkhaa <cryptojavkhlantugs@gmail.com>
2022-08-19 11:58:55 +0800

    ok

--- src/i18next.js
+++ src/i18next.js
@@ -27,9 +27,6 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    backend: {
-      loadPath: "rentsoestate/locales/{{lng}}/{{ns}}.json",
-    },
   });
 
 export default i18n;
