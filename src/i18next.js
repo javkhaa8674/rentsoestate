05431471e5d2b845356184bfb9f4e352556d2351
cryptojavkhaa <cryptojavkhlantugs@gmail.com>
2022-08-19 10:24:21 +0800

    ok

--- src/i18next.js
+++ src/i18next.js
@@ -27,6 +27,9 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+    backend: {
+      loadPath: "cryptojavkhaa/locales/{{lng}}/{{ns}}.json",
+    },
   });
 
 export default i18n;
